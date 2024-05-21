1a9cd360745d9995e7567e75f16dce45c6200e57
xiao1xiao1fei <2994195008@qq.com>
2024-05-21 22:43:35 +0800

    '数据库配置‘

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'HOST': '127.0.0.1', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'itheima', # 数据库用户名
+        'PASSWORD': '123456', # 数据库用户密码
+        'NAME': 'blog' # 数据库名字
+    },
 }
 
 
